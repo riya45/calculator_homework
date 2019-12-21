49222b5f2a00cebcf77185c22d7df51cb68b897b
riya45 <56205260+riya45@users.noreply.github.com>
2019-12-20 19:21:08 -0800

    Create calculator.py
    modified file

---
+++ src/calculator.py
@@ -0,0 +1,26 @@
+from CsvReader import CsvReader
+
+def addition(a, b):
+    c = a + b
+    return c
+
+
+def subtraction(a, b):
+    a = int(a)
+    b = int(b)
+    c = b - a
+    return c
+
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
